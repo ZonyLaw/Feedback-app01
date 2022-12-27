c11346a73ce10acc6f59d2b6f4ae20a37c1284f1
Zonylaw <sunny_law@hotmail.com>
2022-12-27 19:47:23 +0000

    add feedback stats to the app

--- src/App.js
+++ src/App.js
@@ -3,18 +3,25 @@ import {useState} from 'react';
 import Header from './components/Header';
 import FeedbackList from './components/FeedbackList';
 import FeedbackData from './data/feebackData';
+import FeedbackStats from './components/FeedbackStats';
 
 
 function App() {
   const [feedbacks, setFeedbacks] = useState(FeedbackData)
   const deleteFeedback = (id) => {
-    console.log('App', id);
+    if (window.confirm('Are you sure you want to delete?')){
+      setFeedbacks(feedbacks.filter((item)=>
+        item.id!==id
+      ))
+
+    }
   }
 
   return (
     <>
       <Header />
-        <div>
+        <div className='container'>
+        <FeedbackStats feedbacks={feedbacks} />
           <FeedbackList 
             feedbacks={feedbacks}
             handleDelete={deleteFeedback}
